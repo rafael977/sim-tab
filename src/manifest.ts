3605ca52bc2ed66171302c365367c501a37f5000
Shen Yaoxin <shen_yaoxin@tech.gov.sg>
2022-11-25 14:25:25 +0800

    init

---
+++ src/manifest.ts
@@ -0,0 +1,30 @@
+import pkg from "../package.json"
+
+const manifest = {
+  manifest_version: 2,
+  name: pkg.displayName,
+  short_name: pkg.name,
+  description: pkg.description,
+  version: pkg.version,
+  applications: {
+    gecko: {
+      id: "extension@simtab.io",
+      strict_min_version: "54.0",
+    },
+  },
+  chrome_settings_overrides: {
+    homepage: "./src/index.html",
+  },
+  chrome_url_overrides: {
+    newtab: "./src/index.html",
+  },
+  background: {
+    scripts: ["./src/background/index.js"],
+    persistent: false,
+  },
+  permissions: ["storage", "topSites"],
+}
+
+export default function getManifest() {
+  return manifest
+}
